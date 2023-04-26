26e4510b374b2e5a2f11bf0d6d2eec6000ef7e66
phakkhaphong <phakkhaphong@msn.com>
2023-04-26 12:55:00 +0700

    Create 02.Simulation.sql

---
+++ BackupRestore/02.Simulation.sql
@@ -0,0 +1,67 @@
+USE Master;
+--Full Backup Sat 22:00
+BACKUP DATABASE [AdventureWorks] TO  [MyDevice] 
+WITH FORMAT, INIT,  MEDIANAME = N'My Media';
+GO
+
+--TLOG Backup Mon 9:00-18:00
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+
+WAITFOR DELAY '00:00:10';
+
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+
+WAITFOR DELAY '00:00:10'
+
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+GO
+
+--Diff Mon 22:00
+BACKUP DATABASE [AdventureWorks] TO  [MyDevice] 
+WITH DIFFERENTIAL,  MEDIANAME = N'My Media';
+
+--TLOG Backup Tue 9:00-18:00
+
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+
+--Diff Tue 22:00
+USE AdventureWorks
+GO
+
+INSERT INTO Person.ContactType([Name])
+VALUES ('IT Auditor');
+
+USE Master;
+GO
+BACKUP DATABASE [AdventureWorks] TO  [MyDevice] 
+WITH DIFFERENTIAL,  MEDIANAME = N'My Media';
+
+--TLOG Backup Wed 9:00-18:00
+
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+BACKUP LOG [AdventureWorks] TO  [MyDevice] 
+WITH MEDIANAME = N'My Media';
+
+----Update Person.Person 12:14
+USE AdventureWorks;
+UPDATE Person.Person
+SET ModifiedDate=GETDATE()
+----Crash 12:30
+
+/* STEP 2 
+	1. Stop Service
+	2. Delete Data File (.mdf)
+	3. Start Service
+	4. Backup Tail-Log
+ STEP 3 Restore
+
+ */
